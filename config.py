4e3bb5cba472986b0f2299a4049498151b5595e2
backfromsolaris <porraszach@gmail.com>
2021-08-08 13:38:52 -0500

    signed up, logged in, viewing blog.

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config():
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Good luck debugging this project...'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URL')
+    SECRET_KEY = 'Good luck debugging this project...'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEPLOY_DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
